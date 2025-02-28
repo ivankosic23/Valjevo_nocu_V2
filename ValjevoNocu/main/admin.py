c5794c1aeb39ce5578dff8f1473def8faaf12e8c
ivankosic23 <ivankosic23@gmail.com>
2025-02-28 18:59:57 +0100

    Update#2
    vidi se ID is admin page

--- ValjevoNocu/main/admin.py
+++ ValjevoNocu/main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Svirka,Kafic
 # Register your models here.
+class kafic(admin.ModelAdmin):
+    list_display=('ID','Ime','photo')
 admin.site.register(Svirka)
-admin.site.register(Kafic)
+admin.site.register(Kafic,kafic)
