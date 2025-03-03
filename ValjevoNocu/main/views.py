f44b55f7259d563734681b5b70487bc6fcb1a644
ivankosic23 <ivankosic23@gmail.com>
2025-03-03 03:41:55 +0100

    ovo ce me ubiti
    mobilna verzija nije gotova jos uvek ovo me zajebava namerno

--- ValjevoNocu/main/views.py
+++ ValjevoNocu/main/views.py
@@ -37,18 +37,37 @@ def home(request):
         danpom=(today + timedelta(days=i))
         dayindex = danpom.strftime("%Y-%m-%d")
         dani.append((dayi,dayindex))
-        selected_day = request.GET.get('datum', daysi)
+        day_index = int(request.GET.get('day_index',0))
+
+    selected_day = request.GET.get('datum', daysi)
+
+    if day_index < 0:
+            day_index = len(dani) - 1  # Set to last index
+    elif day_index >= len(dani):
+        day_index = 0
+
+    try:
         date_object = datetime.strptime(selected_day, "%Y-%m-%d").date()
         danilabel=date_object.weekday()
         danilab=days_serbian_padezi[danilabel]
-        svirkes = Svirka.objects.select_related('kafic').filter(datum=daysi)
-        if selected_day is not None:
+    except ValueError:
+         danilab = "Invalid date"
+
+    
+    current_label=dani[day_index][0]
+    day_date=dani[day_index][1]
+
+    svirkes = Svirka.objects.select_related('kafic').filter(datum=daysi)
+    if selected_day is not None:
              svirkes = Svirka.objects.select_related('kafic').filter(datum=selected_day)
     #Kontejneri sa kaficima
 
     return render(request,"home.html",{'days':dani,
                                        'svirke':svirkes,
-                                       'danlabel':danilab})
+                                       'danlabel':danilab,
+                                       'day_index':day_index,
+                                       'current_label':current_label,
+                                       'day_date':day_date})
 
 @login_required
 def dogadjaj(request):
