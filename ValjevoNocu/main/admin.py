7d7c28e4a16794ae44b2d605004cf93245f3b484
ivankosic23 <ivankosic23@gmail.com>
2025-03-18 23:51:05 +0100

    99.9% gotovooo

--- ValjevoNocu/main/admin.py
+++ ValjevoNocu/main/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Svirka,Kafic
 # Register your models here.
 class kafic(admin.ModelAdmin):
-    list_display=('Ime','ID','photo','iglink','walink')
+    list_display=('Ime','ID','photo','iglink','walink','br_mesta','radno_vreme')
 admin.site.register(Svirka)
 admin.site.register(Kafic,kafic)
